48237c72ed86a00b3f32b0d157776874675a51b8
Doberer, Johannes <johannes.doberer@sap.com>
2015-01-15 18:21:56 +0100

    changed download link

--- builder.rb
+++ builder.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Builder < Formula
   homepage 'https://github.com/hybris/homebrew-builder'
   head 'https://github.com/hybris/homebrew-builder'
-  url 'https://github.com/hybris/homebrew-builder/blob/master/builder-sdk-cli/builder-cli.tgz?raw=true'
+  url 'https://github.com/hybris/homebrew-builder/raw/master/builder-sdk-cli/builder.tgz'
   version '0.0.1'
   sha1 'b8bdf4d1b5b6dc6be5953a83537b68f11950bf3d'
     
