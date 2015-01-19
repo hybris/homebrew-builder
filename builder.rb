0856d36837e6a742526f4a50fcc54ca4aa6ee458
Doberer, Johannes <johannes.doberer@sap.com>
2015-01-19 11:01:30 +0100

    sha1 value updated

--- builder.rb
+++ builder.rb
@@ -5,7 +5,7 @@ class Builder < Formula
   head 'https://github.com/hybris/homebrew-builder'
   url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/builder.tgz'
   version '0.0.1'
-  sha1 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+  sha1 '90ec64d4460141b7644fcf6d881e25de5b8beb8d'
     
   depends_on :arch => :x86_64
 
