b211e1576ad2fdfedaf76f095374989a6fd79e40
Doberer, Johannes <johannes.doberer@sap.com>
2015-01-19 11:00:20 +0100

    fix path

--- builder.rb
+++ builder.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Builder < Formula
   homepage 'https://github.com/hybris/homebrew-builder'
   head 'https://github.com/hybris/homebrew-builder'
-  url 'file://usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli'
+  url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/builder.tgz'
   version '0.0.1'
   sha1 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
     
