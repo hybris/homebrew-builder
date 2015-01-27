baa70967a484140000ec2e48f2a6763e64aecc3d
Doberer, Johannes <johannes.doberer@sap.com>
2015-01-27 18:49:55 +0100

    new baas-cli

--- builder.rb
+++ builder.rb
@@ -4,8 +4,8 @@ class Builder < Formula
   homepage 'https://github.com/hybris/homebrew-builder'
   head 'https://github.com/hybris/homebrew-builder'
   url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/builder.tgz'
-  version '1.3-SNAPSHOT'
-  sha1 '90ec64d4460141b7644fcf6d881e25de5b8beb8d'
+  version '1.4-SNAPSHOT'
+  sha1 '062113257f677d850044cf336365a1b6bb83fcc1'
     
   depends_on :arch => :x86_64
 
