280f222da5b7f8b5e5a26e362613725fceb0f609
Stash Writeable <beegees@hybris.com>
2015-02-02 16:00:25 +0100

    release

--- builder.rb
+++ builder.rb
@@ -4,8 +4,8 @@ class Builder < Formula
   homepage 'https://github.com/hybris/homebrew-builder'
   head 'https://github.com/hybris/homebrew-builder'
   url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/builder.tgz'
-  version '1.4'
-  sha1 '819c7aba4914438f1962be357fa423336d7b28b6'
+  version '1.4.0'
+  sha1 '71d1b380fb3add10d69a008440b63b63c573535d'
     
   depends_on :arch => :x86_64
 
