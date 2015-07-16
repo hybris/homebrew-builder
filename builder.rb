c352d1fd71f69a8b6164a526ef0ea8258cd34875
Pracht, Philipp <philipp.pracht@sap.com>
2015-07-16 14:53:59 +0200

    2.0.0

--- builder.rb
+++ builder.rb
@@ -4,8 +4,8 @@ class Builder < Formula
   homepage 'https://github.com/hybris/homebrew-builder'
   head 'https://github.com/hybris/homebrew-builder'
   url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/builder.tgz'
-  version '1.5.0'
-  sha1 'cd0f8dd21b0566344d458893bccd9f633be335bd'
+  version '2.0.0'
+  sha1 'e8a36f2d4fb74acb03f0852d11e05fcfaea1f80a'
     
   depends_on :arch => :x86_64
 
