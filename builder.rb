8278c6bd395f6f914fb48cf60e56fe75c7d36934
Doberer, Johannes <johannes.doberer@sap.com>
2015-09-03 16:15:43 +0200

    fixed release bug

--- builder.rb
+++ builder.rb
@@ -5,7 +5,7 @@ class Builder < Formula
   head 'https://github.com/hybris/homebrew-builder'
   url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/builder.tgz'
   version '2.1.1'
-  sha1 'c5dd41536a4af68ae14e9f1580fc88adb64cbec7'
+  sha1 'b73d479970501b0a3ad84ccbc39c941803ada15f'
     
   depends_on :arch => :x86_64
 
