3355894c03a4a1b0b1f1f315968c419df3faa030
Doberer, Johannes <johannes.doberer@sap.com>
2015-09-04 11:43:14 +0200

    fixed service creation bug

--- builder.rb
+++ builder.rb
@@ -4,8 +4,8 @@ class Builder < Formula
   homepage 'https://github.com/hybris/homebrew-builder'
   head 'https://github.com/hybris/homebrew-builder'
   url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/builder.tgz'
-  version '2.1.1'
-  sha1 'b73d479970501b0a3ad84ccbc39c941803ada15f'
+  version '2.1.2'
+  sha1 'c0d90524f1d17081d2f5c79c387340a267474ec6'
     
   depends_on :arch => :x86_64
 
