b998217276969a277857263687d201120a6606b0
JohannesDoberer <johannes.doberer@sap.com>
2015-09-18 12:11:54 +0200

    fix

--- builder.rb
+++ builder.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Builder < Formula
   homepage 'https://github.com/SAP/builder-cli/'
-  head 'https://github.com/SAP/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
-  url  'https://github.com/SAP/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
+  head 'https://github.com/hybris/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
+  url  'https://github.com/hybris/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
   version '2.1.3'
   sha1 '3ece0cca420609e24a7afc9cee54aa895c2caefd'
 
