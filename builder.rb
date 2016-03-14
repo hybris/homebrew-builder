1596a454485d3765b7cdf95b81cecade6f3e0c50
Kurajsky, Peter <peter.kurajsky@sap.com>
2016-03-14 14:31:57 +0100

    home brew checksum hotfix

--- builder.rb
+++ builder.rb
@@ -5,7 +5,7 @@ class Builder < Formula
   head 'https://github.com/hybris/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
   url  'https://github.com/hybris/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
   version '3.0.2'
-  sha1 '8c2633edece542e705511e60393fbb69b989b3cc'
+  sha1 'f6235e75a2c262a6af69780c757bbb0ff11c4217'
 
   def install
       bin.install 'builder'
