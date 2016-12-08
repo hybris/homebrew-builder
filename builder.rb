d1874311c2968ac6d4a4ced2805f188442575d24
JohannesDoberer <johannes.doberer@sap.com>
2016-12-08 16:34:03 +0100

    release 3.0.8

--- builder.rb
+++ builder.rb
@@ -5,7 +5,7 @@ class Builder < Formula
   head 'https://github.com/hybris/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
   url  'https://github.com/hybris/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
   version '3.0.8'
-  sha256 '0bb9763d86fd5c20bad80f216d848069b4bef70463ae5dc4e306a3228ed58bc4'
+  sha256 '91ddea717f151a1eb9c7d4ed56d2274ca5987a160da8a71894893884dc05d95f'
 
   def install
       bin.install 'builder'
