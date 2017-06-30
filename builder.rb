ff50a542e98c31c0d5e1fcd2ebd88c238d8354de
JohannesDoberer <johannes.doberer@sap.com>
2017-06-30 15:23:43 +0200

    release 3.20

--- builder.rb
+++ builder.rb
@@ -4,8 +4,8 @@ class Builder < Formula
   homepage 'https://github.com/SAP/builder-cli/tree/master/dist/homebrew-builder'
   head 'https://github.com/hybris/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
   url  'https://github.com/hybris/builder-cli/blob/master/dist/homebrew-builder/builder.tgz?raw=true'
-  version '3.19.0'
-  sha256 'c217a9db2c03891beba7731ccd43a0bf83cfd9f5a1b3bb561504a39d8f551cd0'
+  version '3.20.0'
+  sha256 '1bb62c8d15f5142d4e4eee2388be59d367225070dd077b9b06fda4345aa7e5bd'
 
   def install
       bin.install 'builder'
